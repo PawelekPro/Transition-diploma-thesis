d0952460dd676cba1163bac9c265869321d2d2b3
PawelekPro <pawel.gilewicz1@gmail.com>
2023-02-02 20:45:32 +0100

    graphic processing of charts

--- MinMaxAvg.py
+++ MinMaxAvg.py
@@ -27,9 +27,9 @@ def plotIMG(path):
     leftAngle = df['Col1'].to_list()
     rightAngle = df['Col2'].to_list()
 
-    ratio = GLOB_PATH[-7:-5]
+    ratio = GLOB_PATH[-8:-5]
     frequency = BASE_FREQ * (int(ratio) / 100)
-    amplitude = (int(GLOB_PATH[-11:-9]) / 100) * AMPLITUDE
+    amplitude = (int(GLOB_PATH[-12:-10]) / 100) * AMPLITUDE
 
     # Average plot
     leftAng = InterpolatedUnivariateSpline(time, leftAngle)
@@ -41,7 +41,7 @@ def plotIMG(path):
     print(100*(np.mean(leftAngle) - qq_left)/np.mean(leftAngle),
           100*(np.mean(rightAngle) - qq_right)/np.mean(rightAngle))
 
-    return [frequency, min(leftAngle), max(leftAngle), min(rightAngle), max(rightAngle), qq_left, qq_right]
+    return [frequency, min(leftAngle), max(leftAngle), min(rightAngle), max(rightAngle), qq_left, qq_right, amplitude]
 
 if __name__ == "__main__":
     GLOB_PATH = "D:/praca_magisterska/master-s-thesis"
@@ -53,6 +53,7 @@ if __name__ == "__main__":
     right_max = []
     avg_left = []
     avg_right = []
+    amplitude = 0
 
     for path in Path(GLOB_PATH).glob("*.csv"):
         data = plotIMG(path)
@@ -63,41 +64,49 @@ if __name__ == "__main__":
         right_max.append(data[4])
         avg_left.append(data[5])
         avg_right.append(data[6])
+        amplitude = data[7]
 
     # plot 1
+    plt.figure(figsize=(20, 10))
     plt.subplot(1, 2, 1)
     plt.errorbar(frequency, avg_left,
                  [[i - j for i, j in zip(avg_left, left_min)], [j - i for i, j in zip(avg_left, left_max)]],
                  fmt='o', linewidth=2, capsize=6)
     plt.xlim([min(frequency)-0.5, max(frequency)+0.5])
     plt.ylim([0, 140])
-    plt.title("LEFT ANGLE")
+    plt.title("Left angle")
     plt.xlabel("Frequency [Hz]", size=14)
     plt.ylabel('[min(\u03F4{}), avg(\u03F4{}), max(\u03F4{})]'.format(get_sub('L'), get_sub('L'), get_sub('L')), size=14)
     plt.grid(visible=True, which='both', linestyle='--', linewidth='0.25')
     for a, b in zip(frequency, avg_left):
-        plt.text(a, b, str("{:.2f}".format(b)), size=11)
+        plt.text(a, b, str('  ' + "{:.2f}".format(b)), size=9)
     for a, b in zip(frequency, left_min):
-        plt.text(a, b, str("{:.2f}".format(b)), size=11)
+        plt.text(a, b, str('  ' + "{:.2f}".format(b)), size=9)
     for a, b in zip(frequency, left_max):
-        plt.text(a, b, str("{:.2f}".format(b)), size=11)
+        plt.text(a, b, str('  ' + "{:.2f}".format(b)), size=9)
+    HATCH_BOUNDARY = 17
+    plt.fill_between([HATCH_BOUNDARY, max(frequency) + 1], 140, color="none", hatch="/", edgecolor="grey")
 
     # plot 2
     plt.subplot(1, 2, 2)
     plt.errorbar(frequency, avg_right,
                  [[i - j for i, j in zip(avg_right, right_min)], [j - i for i, j in zip(avg_right, right_max)]],
                  fmt='o', linewidth=2, capsize=6)
-    plt.xlim([min(frequency) - 0.5, max(frequency) + 0.5])
+    plt.xlim([min(frequency) - 0.5, max(frequency) + 1])
     plt.ylim([0, 140])
-    plt.title("RIGHT ANGLE")
+    plt.title("Right angle")
     plt.xlabel("Frequency [Hz]", size=14)
     plt.ylabel('[min(\u03F4{}), avg(\u03F4{}), max(\u03F4{})]'.format(get_sub('P'), get_sub('P'), get_sub('P')), size=14)
     plt.grid(visible=True, which='both', linestyle='--', linewidth='0.25')
     for a, b in zip(frequency, avg_right):
-        plt.text(a, b, str("{:.2f}".format(b)), size=11)
+        plt.text(a, b, str('  ' + "{:.2f}".format(b)), size=9)
     for a, b in zip(frequency, right_min):
-        plt.text(a, b, str("{:.2f}".format(b)), size=11)
+        plt.text(a, b, str('  ' + "{:.2f}".format(b)), size=9)
     for a, b in zip(frequency, right_max):
-        plt.text(a, b, str("{:.2f}".format(b)), size=11)
+        plt.text(a, b, str('  ' + "{:.2f}".format(b)), size=9)
+    plt.fill_between([HATCH_BOUNDARY, max(frequency) + 1], 140, color="none", hatch="/", edgecolor="grey")
+
+    plt.suptitle('[min(\u03F4), avg(\u03F4), max(\u03F4)](f)' + ' for forcing amplitude: ' + str(amplitude) + '[mm]', size=16)
+    plt.savefig(str('MinMaxAvg'+str(amplitude)+'.png'), dpi=100)
 
     plt.show()
